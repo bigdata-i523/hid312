6ca9db6d42885e1f5debcaf0e2eae68514dc6334
Neil <nreliaso@iu.edu>
2017-12-03 02:22:15 -0500

    Adding necessary install shell script

---
+++ project/make_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install tk-dev
+sudo apt-get install python3-pip
+sudo apt-get install python3-tk
+python3 /usr/bin/pip3 install matplotlib
+python3 /usr/bin/pip3 install pandas
+python3 /usr/bin/pip3 install numpy
